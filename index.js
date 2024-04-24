1207981aac3f32a5566d89c700ceae42a32eab04
Abdulloh Xo'jayev <xodjaevabdulloh54@gmail.com>
2024-04-24 11:39:23 +0500

    das

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 let son1 = Number(prompt("Birinchi sinni kiriting: "));
 let son2 = Number(prompt("Ikkinchi sinni kiriting: "));
-let amal = Number(prompt("Amalni kiriting(+,-,*,/) "));
+let amal = String(prompt("Amalni kiriting (+, -, *, /,): "));
 
 if (amal === "+") {
     let yigindi = son1 + son2;
-    alert(son1 + "+" + son2 + "=" + yigindi);
+    alert(son1 + " + " + son2 + " = " + yigindi); 
 }
 
 if (amal === "-") {
     let yigindi = son1 - son2;
-    alert(son1 - "-" - son2 - "=" - yigindi);
+    alert(son1 + " - " + son2 + " = " + yigindi);
 }
 
 if (amal === "*") {
     let yigindi = son1 * son2;
-    alert(son1 * "*" * son2 * "=" * yigindi);
+    alert(son1 + " * " + son2 + " = " + yigindi);
 }
 
 if (amal === "/") {
     let yigindi = son1 / son2;
-    alert(son1 / "/" + son2 / "=" / yigindi);
+    alert(son1 + " / " + son2 + " = " + yigindi);
 }
